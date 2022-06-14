cd7e4cab4cadb767b428a425344b2cb2f6a278fc
nicholasg1997 <63173211+nicholasg1997@users.noreply.github.com>
2022-06-14 11:31:51 -0600

    cleaned up code and reformatted

--- BlackJack/BlackJack_betting-HLI.py
+++ BlackJack/BlackJack_betting-HLI.py
@@ -16,71 +16,66 @@ from stable_baselines3 import PPO
 # In[ ]:
 
 
-
-
-
 # In[216]:
 
 
 class BlackJack(gym.Env):
-    
     metadata = {'render.modes': ['console']}
-    #5 different betting multiplers
+    # 5 different betting multiplers
     STAY = 0
     HIT = 1
-    
+
     def __init__(self, decks=1, wallet=2000):
         super(BlackJack, self).__init__()
-        #model takes players hand sum, players first card, and low, medium, high cards percentage
+        # model takes players hand sum, players first card, and low, medium, high cards percentage
         self.betting_model = PPO.load('data/PPO_BlackJack_Hit_Stay')
-        #prep game deck based on howmany decks in play (default=1)
+        # prep game deck based on howmany decks in play (default=1)
         self.decks = decks
         self.deck = self.make_deck()
-        #empty both dealer and players hand
+        # empty both dealer and players hand
         self.player_hand = []
         self.dealer_hand = []
         self.dealer_hand.append(self.draw())
-        #setup action spaces
+        # setup action spaces
         # 2 actions, a small bet, and a big bet
         self.n_actions = 6
         self.action_space = spaces.Discrete(self.n_actions)
-        #12 observations are players hand, dealers face up card, every cards percentage
+        # 12 observations are players hand, dealers face up card, every cards percentage
         self.observation_space = spaces.Box(low=-1000, high=1000,
-                                        shape=(1,), dtype=np.float32)
+                                            shape=(1,), dtype=np.float32)
 
-        
-        
     def reset(self):
         """reset the game by rebuilding the deck, 
         emptying both the player and dealers hand,
         setting dealer face up card to None,
         """
-        #self.deck = self.make_deck()
+        # self.deck = self.make_deck()
         self.player_hand = []
         self.dealer_hand = []
         card = self.draw()
         self.dealer_hand.append(card)
-        
+
         hli_index = self.calculate_ratio()
         np.reshape(hli_index, (1,))
-        
+
         return np.array(np.reshape(hli_index, (1,)), dtype=np.float32)
-        
-    #make the deck of cards    
+
+    # make the deck of cards
     def make_deck(self):
-        deck = [i for i in range(1,10)]*4
-        face_cards = [10,10,10,10] * 4
+        deck = [i for i in range(1, 10)] * 4
+        face_cards = [10, 10, 10, 10] * 4
         deck = deck + face_cards
         deck = deck * self.decks
         return deck
-    #draw a card from the deck then remove it from the deck
+
+    # draw a card from the deck then remove it from the deck
     def draw(self):
         card = random.choice(self.deck)
         self.deck.remove(card)
         return card
-    
+
     def calculate_ratio(self):
-        #calculte high med low cards percentage
+        # calculte high med low cards percentage
         one = self.deck.count(1)
         two = self.deck.count(2)
         three = self.deck.count(3)
@@ -91,66 +86,65 @@ class BlackJack(gym.Env):
         eight = self.deck.count(8)
         nine = self.deck.count(9)
         ten = self.deck.count(10)
-        low = sum([two,three,four,five, six])
+        low = sum([two, three, four, five, six])
         med = sum([seven, eight, nine])
         high = sum([ten, one])
-        r = (52 *self.decks) - (low+med+high)
+        r = (52 * self.decks) - (low + med + high)
         try:
-            hli_index = 100 * ((low-high)/r)
+            hli_index = 100 * ((low - high) / r)
         except  ZeroDivisionError:
-            #print(len(self.deck))
+            # print(len(self.deck))
             hli_index = 0
         np.reshape(hli_index, (1,))
         return hli_index
-    
+
     def step(self, action):
         self.reset()
         bet = action
         done = False
         ####
-        one = round(self.deck.count(1)/len(self.deck),2)
-        two = round(self.deck.count(2)/len(self.deck),2)
-        three = round(self.deck.count(3)/len(self.deck),2)
-        four = round(self.deck.count(4)/len(self.deck),2)
-        five = round(self.deck.count(5)/len(self.deck),2)
-        six = round(self.deck.count(6)/len(self.deck),2)
-        seven = round(self.deck.count(7)/len(self.deck),2)
-        eight = round(self.deck.count(8)/len(self.deck),2)
-        nine = round(self.deck.count(9)/len(self.deck),2)
-        ten = round(self.deck.count(10)/len(self.deck),2)
-        low = sum([two,three,four,five])
-        med = sum([six,seven])
-        high = sum([eight,nine,ten, one])
+        one = round(self.deck.count(1) / len(self.deck), 2)
+        two = round(self.deck.count(2) / len(self.deck), 2)
+        three = round(self.deck.count(3) / len(self.deck), 2)
+        four = round(self.deck.count(4) / len(self.deck), 2)
+        five = round(self.deck.count(5) / len(self.deck), 2)
+        six = round(self.deck.count(6) / len(self.deck), 2)
+        seven = round(self.deck.count(7) / len(self.deck), 2)
+        eight = round(self.deck.count(8) / len(self.deck), 2)
+        nine = round(self.deck.count(9) / len(self.deck), 2)
+        ten = round(self.deck.count(10) / len(self.deck), 2)
+        low = sum([two, three, four, five])
+        med = sum([six, seven])
+        high = sum([eight, nine, ten, one])
         ####
-        
+
         while not done:
-            #get observation space
+            # get observation space
             if len(self.deck) < 15:
                 self.deck = self.make_deck()
             hli_index = self.calculate_ratio()
             np.reshape(hli_index, (1,))
-            #generate observation for hit-stay model
+            # generate observation for hit-stay model
             obs = np.array([sum(self.player_hand), self.dealer_hand[0], low, med, high], dtype=np.float32)
-            #get action from model
+            # get action from model
             action_, _ = self.betting_model.predict(obs, deterministic=True)
-            
+
             if action_ == self.HIT:
                 if len(self.deck) < 15:
                     self.deck = self.make_deck()
                 card = self.draw()
                 self.player_hand.append(card)
                 if 1 in self.player_hand:
-                    if sum(self.player_hand) + 10 <=21:
+                    if sum(self.player_hand) + 10 <= 21:
                         self.player_hand.remove(1)
                         self.player_hand.append(11)
                 if 11 in self.player_hand:
                     if sum(self.player_hand) > 21:
                         self.player_hand.remove(11)
                         self.player_hand.append(1)
-                
-            
+
             if action_ == self.STAY:
-                #dealer plays until hand total is greater then or equal to 17
+                # dealer plays until hand total is greater then or equal to 17
                 while sum(self.dealer_hand) < 17:
                     if sum(self.dealer_hand) > sum(self.player_hand):
                         break
@@ -158,7 +152,7 @@ class BlackJack(gym.Env):
                         card = self.draw()
                         self.dealer_hand.append(card)
                     if 1 in self.dealer_hand:
-                        if sum(self.dealer_hand) + 10 <=21:
+                        if sum(self.dealer_hand) + 10 <= 21:
                             self.dealer_hand.remove(1)
                             self.dealer_hand.append(11)
                     if 11 in self.dealer_hand:
@@ -167,42 +161,40 @@ class BlackJack(gym.Env):
                             self.dealer_hand.append(1)
                     if len(self.deck) < 15:
                         self.deck = self.make_deck()
-                
+
                 done = True
-        
+
         # decide who won the game       
         if sum(self.player_hand) <= 21:
             if sum(self.player_hand) == sum(self.dealer_hand):
-                #tied
+                # tied
                 win = 0
             elif sum(self.player_hand) > sum(self.dealer_hand) or sum(self.dealer_hand) > 21:
                 win = 1
             else:
-                #lost
+                # lost
                 win = -1
         else:
-            #went bust
+            # went bust
             win = -1
-         
+
         # calculate rewards
-        #if player loses
+        # if player loses
         if win == -1:
             reward = bet * win
         # if player wins
         elif win == 1:
             reward = (bet * win) - (self.n_actions - 1)
         # player draws
-        else: 
+        else:
             reward = 0
- 
-        
-        info = {'player hand total:':self.player_hand,
-               'dealer hand total:':self.dealer_hand,
-               'length deck:':len(self.deck),
-                'win:':win,
-               'action:':bet}
-        
-        
+
+        info = {'player hand total:': self.player_hand,
+                'dealer hand total:': self.dealer_hand,
+                'length deck:': len(self.deck),
+                'win:': win,
+                'action:': bet}
+
         return np.array(np.reshape(hli_index, (1,)), dtype=np.float32), float(reward), done, info
 
 
@@ -211,35 +203,25 @@ class BlackJack(gym.Env):
 
 bj = BlackJack()
 
-
 # In[218]:
 
 
 a = bj.reset()
 
-
 # In[219]:
 
 
-a,b,c,d = bj.step(0)
-
+a, b, c, d = bj.step(0)
 
 # In[220]:
 
 
 np.shape(a)
 
-
 # In[ ]:
 
 
-
-
-
 # In[ ]:
-
-
-
 
 
 # In[221]:
@@ -251,12 +233,10 @@ env = BlackJack()
 # It will check your custom environment and output additional warnings if needed
 check_env(env)
 
-
 # In[215]:
 
 
 model = PPO('MlpPolicy', env, verbose=1).learn(100_000)
-
 
 # In[205]:
 
@@ -269,19 +249,17 @@ obs = env.reset()
 for i in tqdm(range(20_000)):
     action, _ = model.predict(obs, deterministic=True)
     obs, reward, done, info = env.step(action)
-    p += action* int(info['win:'])
+    p += action * int(info['win:'])
     wins.append(info['win:'])
     actions.append(action)
     profits.append(reward)
-
 
 # In[206]:
 
 
 w = wins.count(1)
 l = wins.count(-1)
-w/(w+l)
-
+w / (w + l)
 
 # In[207]:
 
@@ -289,17 +267,12 @@ w/(w+l)
 print(sum(profits))
 print(actions.count(0))
 
-
 # In[208]:
 
 
 p
 
-
 # In[ ]:
-
-
-
 
 
 # In[ ]:
@@ -311,26 +284,25 @@ p
 # In[ ]:
 
 
-#saving models as they learn for tensorboard
-#not working (kernel keeps dying possible because m1 mac)
+# saving models as they learn for tensorboard
+# not working (kernel keeps dying possible because m1 mac)
 
 
 # In[ ]:
 
 
-#models_dir = 'models/PPO'
-#logdir = 'logs'
+# models_dir = 'models/PPO'
+# logdir = 'logs'
 
-#if not os.path.exists(models_dir):
-    #os.makedirs(models_dir)
-#if not os.path.exists(logdir):
-    #os.makedirs(logdir)
+# if not os.path.exists(models_dir):
+# os.makedirs(models_dir)
+# if not os.path.exists(logdir):
+# os.makedirs(logdir)
 
-#model = PPO('MlpPolicy', env, verbose=0,tensorboard_log=logdir)
+# model = PPO('MlpPolicy', env, verbose=0,tensorboard_log=logdir)
 model = PPO('MlpPolicy', env, verbose=0)
 TIMESTEPS = 10_000
 for i in tqdm(range(1, 30 + 1)):
     model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False)
-    #model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name='PPO')
-    #model.save(f"{models_dir}/{TIMESTEPS * i}")
-
+    # model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name='PPO')
+    # model.save(f"{models_dir}/{TIMESTEPS * i}")
--- BlackJack/BlackJack_hit_stay_v2.py
+++ BlackJack/BlackJack_hit_stay_v2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
-
 
 import numpy as np
 import gym
@@ -11,84 +9,79 @@ import random
 from tqdm import tqdm
 
 
-# In[40]:
-
-
 class BlackJack(gym.Env):
-    
     metadata = {'render.modes': ['console']}
-    #two player actions stay:0 hit:1
+    # two player actions stay:0 hit:1
     STAY = 0
     HIT = 1
-    
+
     def __init__(self, decks=1, wallet=2000):
         super(BlackJack, self).__init__()
-        #prep game deck based on howmany decks in play (default=1)
+        # prep game deck based on howmany decks in play (default=1)
         self.decks = decks
         self.deck = self.make_deck()
-        #empty both dealer and players hand
+        # empty both dealer and players hand
         self.player_hand = []
         self.dealer_hand = []
         self.dealer_hand.append(self.draw())
-        #setup action spaces
-        #there are two actions stay:0 or hit:1
-        n_actions=2
+        # setup action spaces
+        # there are two actions stay:0 or hit:1
+        n_actions = 2
         self.action_space = spaces.Discrete(n_actions)
-        #5 observations are players hand, dealers face up card, low card%, med card%, high card%
+        # 5 observations are players hand, dealers face up card, low card%, med card%, high card%
         self.observation_space = spaces.Box(low=0, high=30,
-                                        shape=(5,), dtype=np.float32)
+                                            shape=(5,), dtype=np.float32)
 
-        
-        
     def reset(self):
         """reset the game by emptying both the player and dealers hand,
         setting dealer face up card to None, profit back to zero,
         """
-        #self.deck = self.make_deck()
+        # self.deck = self.make_deck()
         self.player_hand = []
         self.dealer_hand = []
         card = self.draw()
         self.dealer_hand.append(card)
-        
+
         low, med, high = self.calculate_ratio()
         return np.array([sum(self.player_hand), self.dealer_hand[0], low, med, high], dtype=np.float32)
-        
-    #make the deck of cards    
+
+    # make the deck of cards
     def make_deck(self):
-        deck = [i for i in range(1,10)]*4
-        face_cards = [10,10,10,10] * 4
+        deck = [i for i in range(1, 10)] * 4
+        face_cards = [10, 10, 10, 10] * 4
         deck = deck + face_cards
         deck = deck * self.decks
         return deck
-    #draw a card from the deck then remove it from the deck
+
+    # draw a card from the deck then remove it from the deck
     def draw(self):
         card = random.choice(self.deck)
         self.deck.remove(card)
         return card
-    
+
     def calculate_ratio(self):
-        #calculte high med low cards percentage
-        one = round(self.deck.count(1)/len(self.deck),2)
-        two = round(self.deck.count(2)/len(self.deck),2)
-        three = round(self.deck.count(3)/len(self.deck),2)
-        four = round(self.deck.count(4)/len(self.deck),2)
-        five = round(self.deck.count(5)/len(self.deck),2)
-        six = round(self.deck.count(6)/len(self.deck),2)
-        seven = round(self.deck.count(7)/len(self.deck),2)
-        eight = round(self.deck.count(8)/len(self.deck),2)
-        nine = round(self.deck.count(9)/len(self.deck),2)
-        ten = round(self.deck.count(10)/len(self.deck),2)
-        low = sum([one,two,three])
-        med = sum([four,five,six,seven])
-        high = sum([eight,nine,ten])
+        # calculte high med low cards percentage
+        one = round(self.deck.count(1) / len(self.deck), 2)
+        two = round(self.deck.count(2) / len(self.deck), 2)
+        three = round(self.deck.count(3) / len(self.deck), 2)
+        four = round(self.deck.count(4) / len(self.deck), 2)
+        five = round(self.deck.count(5) / len(self.deck), 2)
+        six = round(self.deck.count(6) / len(self.deck), 2)
+        seven = round(self.deck.count(7) / len(self.deck), 2)
+        eight = round(self.deck.count(8) / len(self.deck), 2)
+        nine = round(self.deck.count(9) / len(self.deck), 2)
+        ten = round(self.deck.count(10) / len(self.deck), 2)
+        low = sum([one, two, three])
+        med = sum([four, five, six, seven])
+        high = sum([eight, nine, ten])
         return low, med, high
-    
+
     def step(self, action):
         if len(self.deck) < 15:
             self.deck = self.make_deck()
         low, med, high = self.calculate_ratio()
         draw = False
-        
+
         if action == self.HIT:
             if len(self.deck) < 15:
                 self.deck = self.make_deck()
@@ -97,9 +90,9 @@ class BlackJack(gym.Env):
             done = False
             if sum(self.player_hand) > 21:
                 done = True
-            
+
         if action == self.STAY:
-            #dealer plays until hand total is greater then or equal to 17
+            # dealer plays until hand total is greater then or equal to 17
             while sum(self.dealer_hand) < 17:
                 if sum(self.dealer_hand) > sum(self.player_hand):
                     break
@@ -108,41 +101,43 @@ class BlackJack(gym.Env):
                     self.dealer_hand.append(card)
                 if len(self.deck) < 15:
                     self.deck = self.make_deck()
-                
+
             done = True
-        
+
         if done:
-            #check that player is lower then 22
+            # check that player is lower then 22
             if sum(self.player_hand) <= 21:
-                #higher total then dealer
+                # higher total then dealer
                 if sum(self.player_hand) > sum(self.dealer_hand) or sum(self.dealer_hand) > 21:
-                    #player won
+                    # player won
                     win = True
-            
+
                 elif sum(self.player_hand) == sum(self.dealer_hand):
-                    #draw
+                    # draw
                     win = False
                     draw = True
                 else:
                     win = False
             else:
-                #player went bust
+                # player went bust
                 win = False
-            reward = 1.0 if win else -1.0 #win:1 loss:-1
+            reward = 1.0 if win else -1.0  # win:1 loss:-1
             if draw:
                 reward = 0.5
-            info = {'player_hand':sum(self.player_hand),
-                    'dealer_hand':sum(self.dealer_hand),
-                    'len deck:':len(self.deck)
-                   }
+            info = {'player_hand': sum(self.player_hand),
+                    'dealer_hand': sum(self.dealer_hand),
+                    'len deck:': len(self.deck)
+                    }
             self.reset()
-            return np.array([sum(self.player_hand), self.dealer_hand[0], low, med, high], dtype=np.float32), reward, done, info
-        
+            return np.array([sum(self.player_hand), self.dealer_hand[0], low, med, high],
+                            dtype=np.float32), reward, done, info
+
         reward = 0.0
-        info = {'player_hand':sum(self.player_hand),
-                'dealer_hand':sum(self.dealer_hand),
-                'len deck:':len(self.deck)}
-        return np.array([sum(self.player_hand), self.dealer_hand[0], low, med, high], dtype=np.float32), reward, done, info
+        info = {'player_hand': sum(self.player_hand),
+                'dealer_hand': sum(self.dealer_hand),
+                'len deck:': len(self.deck)}
+        return np.array([sum(self.player_hand), self.dealer_hand[0], low, med, high],
+                        dtype=np.float32), reward, done, info
 
 
 # In[43]:
@@ -154,11 +149,7 @@ env = BlackJack()
 # It will check your custom environment and output additional warnings if needed
 check_env(env)
 
-
 # In[ ]:
-
-
-
 
 
 # In[44]:
@@ -169,14 +160,13 @@ from stable_baselines3.common.cmd_util import make_vec_env
 
 # Instantiate the env
 env = BlackJack()
-#env = make_vec_env(lambda: env, n_envs=3)
+# env = make_vec_env(lambda: env, n_envs=3)
 
 
 # In[45]:
 
 
 model = PPO('MlpPolicy', env, verbose=0).learn(500_000)
-
 
 # In[48]:
 
@@ -188,37 +178,12 @@ for i in tqdm(range(10_000)):
     obs = env.reset()
     while not done:
         action, _ = model.predict(obs, deterministic=True)
-        #print(f'action: {action}')
         obs, reward, done, info = env.step(action)
-        #print(info)
-        #print(f'reward: {reward}')
         wins.append(reward)
-
-
-# In[52]:
-
 
 l = wins.count(-1)
 w = wins.count(1)
 d = wins.count(0.5)
-w/(l+w+d)
+w / (l + w + d)
 
-
-# In[106]:
-
-
-#model.save('data/PPO_BlackJack_Hit_Stay')
-
-
-# In[ ]:
-
-
-action, _ = model.predict(np.array([18, 10]), deterministic=True)
-action
-
-
-# In[8]:
-
-
-#model = PPO.load('data/PPO_BlackJack_Hit_Stay') 
 
